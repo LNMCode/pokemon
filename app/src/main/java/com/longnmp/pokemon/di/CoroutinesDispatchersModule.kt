fa3fafcf08836eeb569fc1bf942137c19966e5c0
Nguyễn Mã Phi Long <longnmp@d-soft.com.vn>
2023-03-06 11:15:06 +0700

    add coroutine module

---
+++ app/src/main/java/com/longnmp/pokemon/di/CoroutinesDispatchersModule.kt
@@ -0,0 +1,33 @@
+package com.longnmp.pokemon.di
+
+import com.longnmp.pokemon.di.qualifiers.DefaultDispatcher
+import com.longnmp.pokemon.di.qualifiers.IoDispatcher
+import com.longnmp.pokemon.di.qualifiers.MainDispatcher
+import com.longnmp.pokemon.di.qualifiers.MainImmediateDispatcher
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+
+@InstallIn(SingletonComponent::class)
+@Module
+object CoroutinesDispatchersModule {
+
+    @DefaultDispatcher
+    @Provides
+    fun providesDefaultDispatcher(): CoroutineDispatcher = Dispatchers.Default
+
+    @IoDispatcher
+    @Provides
+    fun providesIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
+
+    @MainDispatcher
+    @Provides
+    fun providesMainDispatcher(): CoroutineDispatcher = Dispatchers.Main
+
+    @MainImmediateDispatcher
+    @Provides
+    fun providesMainImmediateDispatcher(): CoroutineDispatcher = Dispatchers.Main.immediate
+}
